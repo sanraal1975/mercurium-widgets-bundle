88a17015d7b72c8b8b8f151531cb91de95c1e469
Jordi <jordi@MacBook-Pro-de-Jordi.local>
2023-09-23 13:02:22 +0200

    Recovered ArticleAssetFieldsNormalizer and tests

---
+++ Normalizers/Entities/ArticleAssetFieldsNormalizer.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Comitium5\MercuriumWidgetsBundle\Normalizers\Entities;
+
+use Comitium5\ApiClientBundle\Client\Client;
+use Comitium5\ApiClientBundle\Normalizer\NormalizerInterface;
+use Comitium5\MercuriumWidgetsBundle\Constants\EntityConstants;
+use Comitium5\MercuriumWidgetsBundle\Normalizers\EntityAssetNormalizer;
+use Comitium5\MercuriumWidgetsBundle\Normalizers\EntityImageNormalizer;
+use Exception;
+
+/**
+ * Class ArticleAssetFieldsNormalizer
+ *
+ * @package Comitium5\MercuriumWidgetsBundle\Normalizers\Entities
+ */
+class ArticleAssetFieldsNormalizer implements NormalizerInterface
+{
+    /**
+     * @var Client
+     */
+    private $api;
+
+    /**
+     * @var bool
+     */
+    private $normalizeImage;
+
+    /**
+     * @var bool
+     */
+    private $normalizeVideo;
+
+    /**
+     * @var bool
+     */
+    private $normalizeAudio;
+
+    /**
+     * @param Client $api
+     * @param bool $normalizeImage
+     * @param bool $normalizeVideo
+     * @param bool $normalizeAudio
+     */
+    public function __construct(Client $api, bool $normalizeImage = true, bool $normalizeVideo = false, bool $normalizeAudio = false)
+    {
+        $this->api = $api;
+        $this->normalizeImage = $normalizeImage;
+        $this->normalizeVideo = $normalizeVideo;
+        $this->normalizeAudio = $normalizeAudio;
+    }
+
+    /**
+     * @param array $entity
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function normalize(array &$entity): array
+    {
+        if (empty($entity)) {
+            return [];
+        }
+
+        if ($this->normalizeImage) {
+            $normalizer = new EntityImageNormalizer($this->api, EntityConstants::IMAGE_FIELD_KEY);
+            $entity = $normalizer->normalize($entity);
+        }
+
+        if ($this->normalizeVideo) {
+            $normalizer = new EntityAssetNormalizer($this->api, EntityConstants::VIDEO_FIELD_KEY);
+            $entity = $normalizer->normalize($entity);
+        }
+
+        if ($this->normalizeAudio) {
+            $normalizer = new EntityAssetNormalizer($this->api, EntityConstants::AUDIO_FIELD_KEY);
+            $entity = $normalizer->normalize($entity);
+        }
+
+        return $entity;
+    }
+}
---
+++ Tests/Normalizers/Entities/ArticleAssetFieldsNormalizerTest.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Comitium5\MercuriumWidgetsBundle\Tests\Normalizers\Entities;
+
+use ArgumentCountError;
+use Comitium5\MercuriumWidgetsBundle\Constants\EntityConstants;
+use Comitium5\MercuriumWidgetsBundle\Normalizers\Entities\ArticleAssetFieldsNormalizer;
+use Comitium5\MercuriumWidgetsBundle\Tests\Helpers\TestHelper;
+use Comitium5\MercuriumWidgetsBundle\Tests\MocksStubs\ClientMock;
+use Exception;
+use PHPUnit\Framework\TestCase;
+use TypeError;
+
+/**
+ * Class ArticleAssetFieldsNormalizerTest
+ *
+ * @package Comitium5\MercuriumWidgetsBundle\Tests\Normalizers\Entities
+ */
+class ArticleAssetFieldsNormalizerTest extends TestCase
+{
+    /**
+     * @var ClientMock
+     */
+    private $api;
+
+    /**
+     * @param $name
+     * @param array $data
+     * @param $dataName
+     */
+    public function __construct($name = null, array $data = [], $dataName = "")
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $testHelper = new TestHelper();
+        $this->api = $testHelper->getApi();
+    }
+
+    /**
+     * @return void
+     */
+    public function testConstructThrowsArgumentCountErrorException()
+    {
+        $this->expectException(ArgumentCountError::class);
+
+        $normalizer = new ArticleAssetFieldsNormalizer();
+    }
+
+    /**
+     * @dataProvider constructThrowsTypeErrorException
+     *
+     * @return void
+     */
+    public function testConstructThrowsTypeErrorException($api, $normalizeImage, $normalizeVideo, $normalizeAudio)
+    {
+        $this->expectException(TypeError::class);
+
+        $normalizer = new ArticleAssetFieldsNormalizer($api, $normalizeImage, $normalizeVideo, $normalizeAudio);
+    }
+
+    /**
+     * @return array
+     */
+    public function constructThrowsTypeErrorException(): array
+    {
+        return [
+            [
+                "api" => null,
+                "normalizeImage" => true,
+                "normalizeVideo" => false,
+                "normalizeAudio" => false,
+            ],
+            [
+                "api" => $this->api,
+                "normalizeImage" => null,
+                "normalizeVideo" => false,
+                "normalizeAudio" => false,
+            ],
+            [
+                "api" => $this->api,
+                "normalizeImage" => true,
+                "normalizeVideo" => null,
+                "normalizeAudio" => false,
+            ],
+            [
+                "api" => $this->api,
+                "normalizeImage" => true,
+                "normalizeVideo" => true,
+                "normalizeAudio" => null,
+            ],
+        ];
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testNormalizeThrowsArgumentCountErrorException()
+    {
+        $this->expectException(ArgumentCountError::class);
+
+        $normalizer = new ArticleAssetFieldsNormalizer($this->api);
+        $result = $normalizer->normalize();
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testNormalizeThrowsTypeErrorException()
+    {
+        $this->expectException(TypeError::class);
+
+        $normalizer = new ArticleAssetFieldsNormalizer($this->api);
+        $entity = null;
+        $result = $normalizer->normalize($entity);
+    }
+
+    /**
+     * @dataProvider normalize
+     *
+     * @param $entity
+     * @param $normalizeImage
+     * @param $normalizeVideo
+     * @param $normalizeAudio
+     * @param $expected
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function testNormalize($entity, $normalizeImage, $normalizeVideo, $normalizeAudio, $expected)
+    {
+        $normalizer = new ArticleAssetFieldsNormalizer($this->api, $normalizeImage, $normalizeVideo, $normalizeAudio);
+        $result = $normalizer->normalize($entity);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function normalize(): array
+    {
+        return [
+            [
+                "entity" => [],
+                "normalizeImage" => false,
+                "normalizeVideo" => false,
+                "normalizeAudio" => false,
+                "expected" => []
+            ],
+            [
+                "entity" => [
+                    EntityConstants::ID_FIELD_KEY => 1,
+                    EntityConstants::IMAGE_FIELD_KEY => [
+                        EntityConstants::ID_FIELD_KEY => 1
+                    ]
+                ],
+                "normalizeImage" => true,
+                "normalizeVideo" => false,
+                "normalizeAudio" => false,
+                "expected" => [
+                    EntityConstants::ID_FIELD_KEY => 1,
+                    EntityConstants::IMAGE_FIELD_KEY => [
+                        EntityConstants::ID_FIELD_KEY => 1,
+                        EntityConstants::SEARCHABLE_FIELD_KEY => true,
+                        "orientation" => "is-horizontal"
+                    ]
+                ],
+            ],
+            [
+                "entity" => [EntityConstants::ID_FIELD_KEY => 1],
+                "normalizeImage" => false,
+                "normalizeVideo" => true,
+                "normalizeAudio" => false,
+                "expected" => [EntityConstants::ID_FIELD_KEY => 1],
+            ],
+            [
+                "entity" => [EntityConstants::ID_FIELD_KEY => 1],
+                "normalizeImage" => false,
+                "normalizeVideo" => false,
+                "normalizeAudio" => true,
+                "expected" => [EntityConstants::ID_FIELD_KEY => 1],
+            ]
+        ];
+    }
+}
