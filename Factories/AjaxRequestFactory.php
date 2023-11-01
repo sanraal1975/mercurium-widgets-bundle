9a091be0cd54a3e5413d7e5f7ca34778feb92a2b
Jordi <jordi@MacBook-Pro-de-Jordi.local>
2023-11-01 21:01:17 +0100

    Added AjaxRequest Factory and ValueObject

---
+++ Factories/AjaxRequestFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Comitium5\MercuriumWidgetsBundle\Factories;
+
+use ComitiumSuite\Bundle\CSBundle\Widgets\Core\ValueObjects\AjaxRequestValueObject;
+use Exception;
+
+/**
+ * Class AjaxRequestFactory
+ *
+ * @package ComitiumSuite\Bundle\CSBundle\Widgets\Core\Factory
+ */
+class AjaxRequestFactory
+{
+    /**
+     * @param string $ajaxAction
+     * @param string $widgetClass
+     * @param array $widgetParameters
+     * @param array $widgetParametersMapping
+     * @param array $callParameters
+     * @param string $service
+     * @param string $ajaxEntryPoint
+     *
+     * @return AjaxRequestValueObject
+     * @throws Exception
+     */
+    public function create(
+        string $ajaxAction,
+        string $widgetClass,
+        array  $widgetParameters,
+        array  $widgetParametersMapping = [],
+        array  $callParameters = [],
+        string $service = "comitium5_common_widgets_self_call",
+        string $ajaxEntryPoint = "resolveAjaxAction"
+    ): AjaxRequestValueObject
+    {
+        return new AjaxRequestValueObject(
+            $service,
+            $ajaxEntryPoint,
+            $ajaxAction,
+            $widgetClass,
+            $widgetParameters,
+            $callParameters,
+            $widgetParametersMapping
+        );
+    }
+}
