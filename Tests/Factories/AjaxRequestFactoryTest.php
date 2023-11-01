f350dc8d9782f77f8260fe41035c179a48c05317
Jordi <jordi@MacBook-Pro-de-Jordi.local>
2023-11-01 22:42:35 +0100

    Added AjaxRequestFactoryTest

---
+++ Tests/Factories/AjaxRequestFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Comitium5\MercuriumWidgetsBundle\Tests\Factories;
+
+use Comitium5\MercuriumWidgetsBundle\Constants\EntityConstants;
+use Comitium5\MercuriumWidgetsBundle\Factories\AjaxRequestFactory;
+use Comitium5\MercuriumWidgetsBundle\ValueObjects\AjaxRequestValueObject;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AjaxRequestFactoryTest
+ *
+ * @package Comitium5\MercuriumWidgetsBundle\Tests\Factories
+ */
+class AjaxRequestFactoryTest extends TestCase
+{
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testCreate()
+    {
+        $factory = new AjaxRequestFactory();
+
+        $valueObject = $factory->create(
+            "dummyAction",
+            "dummyClass",
+            [EntityConstants::ID_FIELD_KEY => 1]
+    );
+
+        $this->assertInstanceOf(AjaxRequestValueObject::class, $valueObject);
+    }
+
+}
