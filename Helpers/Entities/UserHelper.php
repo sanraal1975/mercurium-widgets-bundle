f55b7fd1d564ebddb24a5e7129dabe7559a90bd8
Jordi <jordi@MacBook-Pro-de-Jordi.local>
2024-05-03 20:10:37 +0200

    W.I.P.

--- Helpers/Entities/UserHelper.php
+++ Helpers/Entities/UserHelper.php
@@ -23,7 +23,7 @@ class UserHelper
     {
         $helper = new EntityHelper();
         $subscriptions = $helper->getSubscriptions($userData);
-        
+
         return !empty($subscriptions);
     }
 
