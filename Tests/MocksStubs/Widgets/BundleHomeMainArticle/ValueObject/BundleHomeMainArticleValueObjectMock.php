86f02260813ffe48cb38963526968bc999b2f5bf
Jordi <jordi@MacBook-Pro-de-Jordi.local>
2024-05-20 21:38:25 +0200

    Updated tests

--- Tests/MocksStubs/Widgets/BundleHomeMainArticle/ValueObject/BundleHomeMainArticleValueObjectMock.php
+++ Tests/MocksStubs/Widgets/BundleHomeMainArticle/ValueObject/BundleHomeMainArticleValueObjectMock.php
@@ -3,7 +3,6 @@
 namespace Comitium5\MercuriumWidgetsBundle\Tests\MocksStubs\Widgets\BundleHomeMainArticle\ValueObject;
 
 use Comitium5\ApiClientBundle\Client\Client;
-use Comitium5\MercuriumWidgetsBundle\Normalizers\EntityNormalizer;
 use Comitium5\MercuriumWidgetsBundle\Widgets\BundleHomeMainArticle\ValueObject\BundleHomeMainArticleValueObject;
 
 /**
@@ -22,36 +21,6 @@ class BundleHomeMainArticleValueObjectMock extends BundleHomeMainArticleValueObj
      * @var string
      */
     private $articlesIds;
-
-    /**
-     * @var string
-     */
-    private $format;
-
-    /**
-     * @var bool
-     */
-    private $showImage;
-
-    /**
-     * @var bool
-     */
-    private $showNumComments;
-
-    /**
-     * @var bool
-     */
-    private $showRelatedContent;
-
-    /**
-     * @var bool
-     */
-    private $showSponsor;
-
-    /**
-     * @var bool
-     */
-    private $showSubtitle;
 
     /**
      * @param Client $api
@@ -79,52 +48,33 @@ class BundleHomeMainArticleValueObjectMock extends BundleHomeMainArticleValueObj
         return $this->articlesIds;
     }
 
-    /**
-     * @return string
-     */
     public function getFormat(): string
     {
-        return $this->format;
+        // TODO: Implement getFormat() method.
     }
 
-    /**
-     * @return bool
-     */
     public function getShowImage(): bool
     {
-        return $this->showImage;
+        // TODO: Implement getShowImage() method.
     }
 
-    /**
-     * @return bool
-     */
     public function getShowNumComments(): bool
     {
-        return $this->showNumComments;
+        // TODO: Implement getShowNumComments() method.
     }
 
-    /**
-     * @return bool
-     */
     public function getShowRelatedContent(): bool
     {
-        return $this->showRelatedContent;
+        // TODO: Implement getShowRelatedContent() method.
     }
 
-    /**
-     * @return bool
-     */
     public function getShowSponsor(): bool
     {
-        return $this->showSponsor;
+        // TODO: Implement getShowSponsor() method.
     }
 
-    /**
-     * @return bool
-     */
     public function getShowSubtitle(): bool
     {
-        return $this->showSubtitle;
+        // TODO: Implement getShowSubtitle() method.
     }
-
 }
